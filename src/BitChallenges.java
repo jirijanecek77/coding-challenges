b9f8e59bef9826edf248821ab51bfb84f05f2886
Jiří Janeček <jiri.janecek@partners.cz>
2023-03-06 09:45:53 +0100

    count bit challenge

--- src/BitChallenges.java
+++ src/BitChallenges.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 public class BitChallenges {
 
     public static long swapBits(long x, int i, int j) {
@@ -12,7 +17,7 @@ public class BitChallenges {
         return x;
     }
 
-    public static int closestPowerOf( int n) {
+    public static int closestPowerOf(int n) {
         int i = 0, a = 1;
 
         while (a <= n) {
@@ -21,5 +26,30 @@ public class BitChallenges {
         }
 
         return i - 1;
+    }
+
+    public static int[] sortBy1s(int[] arr) {
+        /*
+        Given an integer array arr. You have to sort the integers in the array in ascending order by the number
+        of 1's in their binary representation and in case of two or more integers have the same number of 1's
+        you have to sort them in ascending order.
+         */
+
+        return Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(BitChallenges::countBits))
+                .entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .flatMap(e -> e.getValue().stream().sorted())
+                .mapToInt(i -> i)
+                .toArray();
+    }
+
+    private static int countBits(int n) {
+        int result = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            result++;
+        }
+        return result;
     }
 }
--- test/BitChallengesTest.java
+++ test/BitChallengesTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -19,4 +20,9 @@ class BitChallengesTest {
         assertEquals(3, result);
     }
 
+    @Test
+    void testSortBy1s() {
+        int[] result = BitChallenges.sortBy1s(new int[]{0, 2, 1, 3, 5, 7, 4, 6, 8});
+        assertArrayEquals(new int[]{0, 1, 2, 4, 8, 3, 5, 6, 7}, result);
+    }
 }

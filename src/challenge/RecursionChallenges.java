17967a648517220f68b07ecb322ea7434ef66de8
Jiří Janeček <jiri.janecek@partners.cz>
2023-10-29 11:29:24 +0100

    Sudoku with recursion

--- src/challenge/RecursionChallenges.java
+++ src/challenge/RecursionChallenges.java
@@ -107,4 +107,63 @@ public class RecursionChallenges {
                 .sum()));
     }
 
+
+    static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board.length; column++) {
+                if (board[row][column] == 0) {
+                    for (int numberToTry = 1; numberToTry <= board.length; numberToTry++) {
+                        if (isValidPlacement(board, numberToTry, row, column)) {
+                            board[row][column] = numberToTry;
+                            if (solveSudoku(board)) {
+                                return true;
+
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !numberInRow(board, number, row) &&
+                !numberInColumn(board, number, column) &&
+                !numberInBox(board, number, row, column);
+    }
+
+    private static boolean numberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean numberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean numberInColumn(int[][] board, int number, int column) {
+        for (int[] columns : board) {
+            if (columns[column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

8e669c4a7d4517a2561afad1bdd40d5391470f74
Jiří Janeček <jiri.janecek@partners.cz>
2023-12-12 22:31:20 +0100

    Advent of code - day 12 - part 1

---
+++ src/main/java/advent/year_2023/AdventOfCodeDay12.java
@@ -0,0 +1,82 @@
+package advent.year_2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AdventOfCodeDay12 {
+
+    private static final Pattern ROW_PATTERN = Pattern.compile("^((.|\\?|#)+) ((\\d(,)*)+)$");
+
+    static long combinationsCount(String inputFileName, int repetitions) throws IOException {
+        final BufferedReader reader = Files.newBufferedReader(Paths.get(inputFileName));
+
+        int sum = 0;
+
+        try (reader) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                Matcher matcher = ROW_PATTERN.matcher(line);
+                if (!matcher.find()) {
+                    throw new RuntimeException("unknown row");
+                }
+
+                List<Integer> counts = Collections.nCopies(
+                                repetitions,
+                                Arrays.stream(matcher.group(3).split(",")).map(Integer::parseInt).toList())
+                        .stream().flatMap(Collection::stream).toList();
+
+                String data = IntStream.range(0, repetitions).mapToObj(e -> matcher.group(1)).collect(Collectors.joining("?"));
+                String input = cleanData(data);
+                String target = counts.stream().map("#"::repeat).collect(Collectors.joining("."));
+
+                System.out.println(input + counts);
+                int s = solve(input, counts, target);
+                System.out.println(s);
+                sum += s;
+
+                // read next line
+                line = reader.readLine();
+            }
+
+            return sum;
+        }
+    }
+
+    private static int solve(String data, List<Integer> counts, String target) {
+//        System.out.println(data + counts);
+        int first = data.indexOf('?');
+        if (first < 0) {
+            int result = cleanData(data).equals(target) ? 1 : 0;
+//            System.out.println(result);
+            return result;
+        } else if (!(target + ".").startsWith(data.substring(0, first))) {
+//            System.out.println("cancel " + data + " not like " + target);
+            return 0;
+        }
+        return solve(cleanData(data.replaceFirst("\\?", ".")), counts, target)
+                + solve(cleanData(data.replaceFirst("\\?", "#")), counts, target);
+    }
+
+    private static String cleanData(String data) {
+        return removeTrailingDots(removeLeadingDots(data)).replaceAll("\\.{2,}", ".");
+    }
+
+    private static String removeLeadingDots(String s) {
+        return s.replaceAll("^(\\.)+", "");
+    }
+
+    private static String removeTrailingDots(String s) {
+        return s.replaceAll("(\\.)+$", "");
+    }
+}
---
+++ src/test/java/advent/year_2023/AdventOfCodeDay12Test.java
@@ -0,0 +1,22 @@
+package advent.year_2023;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdventOfCodeDay12Test {
+
+    @Test
+    void combinationsCount() throws IOException {
+        assertEquals(21, AdventOfCodeDay12.combinationsCount("src/test/resources/advent/2023_12/input_test.in", 1));
+        assertEquals(8180, AdventOfCodeDay12.combinationsCount("src/test/resources/advent/2023_12/input.in", 1));
+    }
+
+    @Test
+    void combinationsCount5() throws IOException {
+        assertEquals(525152, AdventOfCodeDay12.combinationsCount("src/test/resources/advent/2023_12/input_test.in", 5));
+//        assertEquals(742305960572L, AdventOfCodeDay12.combinationsCount("src/test/resources/advent/2023_12/input.in", 5));
+    }
+}

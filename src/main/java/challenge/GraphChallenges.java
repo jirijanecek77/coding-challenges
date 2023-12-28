7d662a25a3ec38c39f2dae511c325cc92149c8a9
Jiří Janeček <jiri.janecek@partners.cz>
2023-12-28 12:50:33 +0100

    GraphChallenges

---
+++ src/main/java/challenge/GraphChallenges.java
@@ -0,0 +1,52 @@
+package challenge;
+
+import java.util.*;
+
+public class GraphChallenges {
+
+    public static double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        // https://leetcode.com/problems/evaluate-division/description/?envType=study-plan-v2&envId=top-interview-150
+
+        Map<String, Map<String, Double>> vertices = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++) {
+            var from = equations.get(i).get(0);
+            var to = equations.get(i).get(1);
+            var value = values[i];
+
+            addVertexEdge(vertices, from, to, value);
+            addVertexEdge(vertices, to, from, 1 / value);
+        }
+
+        return queries.stream()
+                .mapToDouble(query -> dfs(query.get(0), query.get(1), vertices, new HashSet<>(), 1.0))
+                .toArray();
+    }
+
+    private static void addVertexEdge(Map<String, Map<String, Double>> vertices, String from, String to, double value) {
+        if (vertices.containsKey(from)) {
+            vertices.get(from).put(to, value);
+        } else {
+            Map<String, Double> edge = new HashMap<>();
+            edge.put(to, value);
+            vertices.put(from, edge);
+        }
+    }
+
+    private static double dfs(String from, String to, Map<String, Map<String, Double>> vertices, Set<String> visited, double acc) {
+        if (!vertices.containsKey(from) || !vertices.containsKey(to) || visited.contains(from)) {
+            return -1.0;
+        }
+        visited.add(from);
+
+        var edges = vertices.get(from);
+        if (edges.containsKey(to)) {
+            return edges.get(to) * acc;
+        }
+
+        return edges.entrySet().stream()
+                .map(e -> dfs(e.getKey(), to, vertices, visited, acc * e.getValue()))
+                .filter(e -> e > 0)
+                .findFirst()
+                .orElse(-1.0);
+    }
+}
---
+++ src/test/java/challenge/GraphChallengesTest.java
@@ -0,0 +1,27 @@
+package challenge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class GraphChallengesTest {
+
+    @Test
+    void calcEquation() {
+        assertArrayEquals(new double[]{6.0, 0.5, -1.0, 1.0, -1.0}, GraphChallenges.calcEquation(
+                        List.of(List.of("a", "b"), List.of("b", "c")),
+                        new double[]{2.0, 3.0},
+                        List.of(List.of("a", "c"), List.of("b", "a"), List.of("a", "e"), List.of("a", "a"), List.of("x", "x"))
+                )
+        );
+
+        assertArrayEquals(new double[]{360.0, 0.008333333333333333, 20.0, 1.0, -1.0, -1.0}, GraphChallenges.calcEquation(
+                        List.of(List.of("x1", "x2"), List.of("x2", "x3"), List.of("x3", "x4"), List.of("x4", "x5")),
+                        new double[]{3.0, 4.0, 5.0, 6.0},
+                        List.of(List.of("x1", "x5"), List.of("x5", "x2"), List.of("x2", "x4"), List.of("x2", "x2"), List.of("x2", "x9"), List.of("x9", "x9"))
+                )
+        );
+    }
+}

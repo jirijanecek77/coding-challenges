c81729cf7aee7b36d4300537ed78ba7edb4748f1
Jiří Janeček <jiri.janecek@gmail.com>
2025-09-25 11:27:40 +0200

    prefix sum challenges

--- src/main/python/prefix_sum/prefix_sum.py
+++ src/main/python/prefix_sum/prefix_sum.py
@@ -11,7 +11,7 @@ def calculate_prefix_sum(nums: list[int]) -> list[int]:
 
 
 def pivot_index(nums: list[int]) -> int:
-    # https://leetcode.com/problems/find-pivot-index/description
+    # https://leetcode.com/problems/find-pivot-index
     prefix_sum = calculate_prefix_sum(nums)
     prefix_sum.insert(0, 0)
     prefix_sum.append(prefix_sum[-1])
@@ -21,3 +21,20 @@ def pivot_index(nums: list[int]) -> int:
             return i - 1
 
     return -1
+
+
+def sum_odd_length_subarrays(arr: list[int]) -> int:
+    # https://leetcode.com/problems/sum-of-all-odd-length-subarrays
+    prefix_sum = calculate_prefix_sum(arr)
+    prefix_sum.insert(0, 0)
+    prefix_sum.append(prefix_sum[-1])
+
+    n = len(prefix_sum)
+    result = 0
+    for i in range(1, n - 1, 2):
+        result += sum(prefix_sum[j] - prefix_sum[j - i] for j in range(i, n - 1))
+    return result
+
+
+if __name__ == "__main__":
+    print(sum_odd_length_subarrays([1, 4, 2, 5, 3]))
